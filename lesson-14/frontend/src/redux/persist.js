20723a2e6b72ee22c59f27d46fdcf397f7e7f6b8
Vova Melnyk <hagmen4339@gmail.com>
2021-08-25 19:34:46 +0300

    add persist

---
+++ lesson-14/frontend/src/redux/persist.js
@@ -0,0 +1,44 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import contactsReducer from "./contactsRedux/contactsReducer";
+import authReducer from "./authRedux/authReducer";
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
+});
+
+let persist = persistStore(store);
+
+export { store, persist };
